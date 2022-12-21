292a73d2b9c856e416dd3e498431fdbb54329d37
coolgoagle <102777764+coolgoagle@users.noreply.github.com>
2022-12-20 19:47:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 function button() {
   document.getElementById("thebutton").remove();
   document.getElementById("rick").play();
+  document.title="Trolled!";
+  document.getElementById("theicon").href="/img/trollface.ico";
 }
